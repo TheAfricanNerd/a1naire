1234259e4b0ee6247aad010c2ee04fba6b80fdb4
Binadiegha <jonesbgabriel@gmail.com>
2024-06-17 17:26:36 +0100

    fix: pdf viewer

--- src/views/Pdf/index.tsx
+++ src/views/Pdf/index.tsx
@@ -1,16 +1,79 @@
 
-import Iframe from "react-iframe";
+import {useCallback, useMemo, useState} from "react";
+// import Iframe from "react-iframe";
+import { pdfjs } from 'react-pdf';
+import { Document, Page } from 'react-pdf';
+import {useResizeObserver} from "@wojtekmaj/react-hooks";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
+
+const options = {
+    cMapUrl: '/cmaps/',
+    standardFontDataUrl: '/standard_fonts/',
+};
+
+
+const resizeObserverOptions = {};
+const maxWidth = 800;
 function PdfViewer(){
+    const file = useMemo(() => ({url:'./src/A1naire-customer-presentation.pdf'}), []);
+
+    const [numPages, setNumPages] = useState<number>();
+    // const [pageNumber, setPageNumber] = useState<number>(1);
+    // const [deviceWidth, setDeviceWidth] = useState<number>()
+    const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+    const [containerWidth, setContainerWidth] = useState<number>();
+
+    const onResize = useCallback<ResizeObserverCallback>((entries) => {
+        const [entry] = entries;
+
+        if (entry) {
+            setContainerWidth(entry.contentRect.width);
+        }
+    }, []);
+
+    useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+        setNumPages(numPages);
+    }
+// ./src/A1naire-customer-presentation.pdf
     return (<>
-        {/*<iframe src="https://www.youtube.com/watch?v=8reaJG7z-is" width={'100%'} height={'100%'}></iframe>*/}
-        <div className='h-screen w-full'>
-            <Iframe url={'https://tunjiafonja.com/A1naire-customer-presentation.pdf'} width="100%"
-                    height="100%"
-                    id=""
-                    className="h-full"
-                    display="block"
-                    position="relative" />
+
+        <div>
+            <div className={'w-full'} ref={setContainerRef}>
+                <Document loading={<div>Loading...</div>} file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+                    {Array.from(new Array(numPages), (_el, index) => (
+                        <Page
+                            key={`page_${index + 1}`}
+                            pageNumber={index + 1}
+                            renderAnnotationLayer={false}
+                            renderTextLayer={false}
+                            width={containerWidth ? Math.max(containerWidth, maxWidth) : maxWidth}
+                        />
+                    ))}
+
+                </Document>
+            </div>
+
+            {/*<p>*/}
+            {/*    Page {numPages} / {pageNumber}*/}
+            {/*</p>*/}
         </div>
+
+        {/*<iframe src="https://www.youtube.com/watch?v=8reaJG7z-is" width={'100%'} height={'100%'}></iframe>*/}
+        {/*<div className='h-screen w-full'>*/}
+        {/*    <Iframe url={'https://tunjiafonja.com/A1naire-customer-presentation.pdf'} width="100%"*/}
+        {/*            height="100%"*/}
+        {/*            id=""*/}
+        {/*            className="h-full"*/}
+        {/*            display="block"*/}
+        {/*            position="relative" />*/}
+        {/*</div>*/}
+
 
     </>)
 }
